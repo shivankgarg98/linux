2911c2f247fd354b4bc51d9456996f57ade35911
Nathan Lynch <nathan.lynch@amd.com>
2025-08-25 17:44:11 -0500

    dmaengine: sdxi: Remove build_admin_start_new(), build_admin_stop_new()
    These now have no users, remove them.
    
    Signed-off-by: Nathan Lynch <nathan.lynch@amd.com>

--- drivers/dma/sdxi/context.c
+++ drivers/dma/sdxi/context.c
@@ -21,32 +21,6 @@
 #include "sdxi.h"
 #include "trace.h"
 
-void build_admin_start_new(struct sdxi_desc *desc, bool vf, u16 vf_num,
-			   u16 cxt_start, u16 cxt_end, u64 doorbell)
-{
-	memset(desc, 0, sizeof(*desc));
-
-	desc->fe = 1;
-	DESC_ADM_BUILD_VF(desc, vf, vf_num);
-	DESC_ADM_BUILD_CXT(desc, cxt_start, cxt_end);
-	desc->body[3] |= lower_32_bits(doorbell);
-	desc->body[4] |= upper_32_bits(doorbell);
-	DESC_BUILD_TYPE(desc, OP_TYPE_ADMIN, OP_ADMIN_START);
-	desc->csb_ptr = 0x1;
-}
-
-void build_admin_stop_new(struct sdxi_desc *desc, bool vf, u16 vf_num,
-			   u16 cxt_start, u16 cxt_end, u64 doorbell)
-{
-	memset(desc, 0, sizeof(*desc));
-
-	desc->fe = 1;
-	DESC_ADM_BUILD_VF(desc, vf, vf_num);
-	DESC_ADM_BUILD_CXT(desc, cxt_start, cxt_end);
-	DESC_BUILD_TYPE(desc, OP_TYPE_ADMIN, OP_ADMIN_STOP);
-	desc->csb_ptr = 0x1;
-}
-
 void build_dma_copy(struct sdxi_desc *desc, u32 size, u8 src_attr,
 		    u8 dst_attr, u16 src_akey, u16 dst_akey,
 		    u64 src_addr, u64 dst_addr)
--- drivers/dma/sdxi/context.h
+++ drivers/dma/sdxi/context.h
@@ -40,10 +40,6 @@
 		(d)->body[2] |= ((mask) & 0xFFFF) << 16;	\
 	} while (0)
 
-void build_admin_start_new(struct sdxi_desc *desc, bool vf, u16 vf_num,
-			   u16 cxt_start, u16 cxt_end, u64 doorbell);
-void build_admin_stop_new(struct sdxi_desc *desc, bool vf, u16 vf_num,
-			   u16 cxt_start, u16 cxt_end, u64 doorbell);
 void build_dma_copy(struct sdxi_desc *desc, u32 size, u8 src_attr,
 		    u8 dst_attr, u16 src_akey, u16 dst_akey,
 		    u64 src_addr, u64 dst_dst);
