1d7b48c797a730c21882aace7917ebb7e7d443cd
Nathan Lynch <nathan.lynch@amd.com>
2025-08-27 13:30:06 -0500

    dmaengine: sdxi: dma: Return -EINVAL on bad copy length
    sdxi_dma_start_desc()'s only caller ignores errors, but return a
    proper error value just in case that ever changes.
    
    Signed-off-by: Nathan Lynch <nathan.lynch@amd.com>

--- drivers/dma/sdxi/dma.c
+++ drivers/dma/sdxi/dma.c
@@ -69,7 +69,7 @@ static int sdxi_dma_start_desc(struct sdxi_dma_desc *dma_desc)
 	sq = cxt->sq;
 
 	if (sdxi_cmd->len > MAX_DMA_COPY_BYTES)
-		return 1;
+		return -EINVAL;
 
 	build_dma_copy(&desc, sdxi_cmd->len, 0, 0, 0, 0, sdxi_cmd->src_addr,
 		       sdxi_cmd->dst_addr);
