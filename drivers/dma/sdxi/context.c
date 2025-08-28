b516a49d4d8b5f5d8a12e6476e538b61efca3f65
Nathan Lynch <nathan.lynch@amd.com>
2025-08-28 18:14:15 -0500

    dmaengine: sdxi: sdxi.h: Move sdxi_working_cxt_init() call tree to context.c
    Now move the rest of the context and L1/L2 table management to
    context.c.
    
    Signed-off-by: Nathan Lynch <nathan.lynch@amd.com>

--- drivers/dma/sdxi/context.c
+++ drivers/dma/sdxi/context.c
@@ -24,6 +24,15 @@
 #include "sdxi.h"
 #include "trace.h"
 
+static bool force_pr_for_user_contexts;
+module_param(force_pr_for_user_contexts, bool, 0644);
+MODULE_PARM_DESC(force_pr_for_user_contexts,
+		 "Force-enable the 'pr' bit for user contexts. "
+		 "Not useful without set_pr_bits=1. "
+		 "This is a security hole and is intended for hardware "
+		 "validation only. "
+		 "(default: false)");
+
 /* Alloc sdxi_sq in kernel space */
 struct sdxi_sq *sdxi_sq_alloc(struct sdxi_cxt *cxt, int ring_entries)
 {
@@ -131,6 +140,326 @@ void sdxi_sq_free(struct sdxi_sq *sq)
 struct sdxi_sq *sdxi_sq_alloc_default(struct sdxi_cxt *cxt)
 {
 	return sdxi_sq_alloc(cxt, DEFAULT_DESC_RING_ENTRIES);
+}
+
+static bool sdxi_cxt_l2_ent_vl(const struct sdxi_cxt_l2_ent *ent)
+{
+	return FIELD_GET(SDXI_CXT_L2_ENT_VL, le64_to_cpu(ent->lv01_ptr));
+}
+
+static dma_addr_t sdxi_cxt_l2_ent_lv01_ptr(const struct sdxi_cxt_l2_ent *ent)
+{
+	return FIELD_GET(SDXI_CXT_L2_ENT_LV01_PTR, le64_to_cpu(ent->lv01_ptr)) << ilog2(SZ_4K);
+}
+
+
+static void set_cxt_l2_entry(struct sdxi_cxt_l2_ent *l2_entry,
+			     dma_addr_t l1_table_dma)
+{
+	u64 lv01_ptr;
+
+	/* We shouldn't be updating a live entry. */
+	if (WARN_ON_ONCE(sdxi_cxt_l2_ent_vl(l2_entry)))
+		return;
+	/* L1 tables must be 4K-aligned. */
+	if (WARN_ON_ONCE(!IS_ALIGNED(l1_table_dma, SZ_4K)))
+		return;
+
+	lv01_ptr = (FIELD_PREP(SDXI_CXT_L2_ENT_LV01_PTR,
+				   l1_table_dma >> ilog2(SZ_4K)) |
+		    FIELD_PREP(SDXI_CXT_L2_ENT_VL, 1));
+
+	// Ensure the valid bit update follows prior updates to other
+	// control structures.
+	dma_wmb();
+	WRITE_ONCE(l2_entry->lv01_ptr, cpu_to_le64(lv01_ptr));
+}
+
+static void set_cxt_l1_entry(struct sdxi_dev *sdxi,
+			     struct sdxi_cxt_l1_ent *l1_entry,
+			     struct sdxi_cxt *cxt)
+{
+	u64 cxt_ctl_ptr;
+	u64 akey_ptr;
+	u16 intr_num;
+	u32 misc0;
+	bool pr;
+
+	if (!cxt) {
+		memset(l1_entry, 0, sizeof(*l1_entry));
+		return;
+	}
+
+	pr = sdxi->use_privileged_bits && cxt->privileged;
+
+	cxt_ctl_ptr = (FIELD_PREP(SDXI_CXT_L1_ENT_VL, 1) |
+		       FIELD_PREP(SDXI_CXT_L1_ENT_KA, 1) |
+		       FIELD_PREP(SDXI_CXT_L1_ENT_PR, pr) |
+		       FIELD_PREP(SDXI_CXT_L1_ENT_CXT_CTL_PTR,
+				  cxt->cxt_ctl_dma >> L1_CXT_CTRL_PTR_SHIFT));
+	akey_ptr = (FIELD_PREP(SDXI_CXT_L1_ENT_AKEY_SZ,
+			       akey_table_order(cxt->akey_table)) |
+		    FIELD_PREP(SDXI_CXT_L1_ENT_AKEY_PTR,
+			       cxt->akey_table_dma >> L1_CXT_AKEY_PTR_SHIFT));
+	misc0 = FIELD_PREP(SDXI_CXT_L1_ENT_MAX_BUFFER, 11);
+
+	*l1_entry = (struct sdxi_cxt_l1_ent) {
+		.cxt_ctl_ptr = cpu_to_le64(cxt_ctl_ptr),
+		.akey_ptr = cpu_to_le64(akey_ptr),
+		.misc0 = cpu_to_le32(misc0),
+		.opb_000_enb = cpu_to_le32(sdxi->op_grp_cap),
+	};
+
+	intr_num = le16_to_cpu(cxt->akey_table->entry[0].intr_num);
+	FIELD_MODIFY(SDXI_AKEY_ENT_VL, &intr_num, 1);
+	cxt->akey_table->entry[0].intr_num = cpu_to_le16(intr_num);
+}
+
+static int config_cxt_tables(struct sdxi_dev *sdxi,
+			     struct sdxi_cxt *cxt)
+{
+	struct sdxi_cxt_l1_table *l1_table;
+	struct sdxi_cxt_l1_ent *l1_entry;
+	u16 l2_idx;
+	u8 l1_idx;
+
+	l2_idx = ID_TO_L2_INDEX(cxt->id);
+	l1_idx = ID_TO_L1_INDEX(cxt->id);
+
+	// Allocate L1 table if not present.
+	l1_table = sdxi->l1_table_array[l2_idx];
+	if (!l1_table) {
+		struct sdxi_cxt_l2_ent *l2_entry;
+		dma_addr_t l1_table_dma;
+
+		l1_table = dma_alloc_coherent(sdxi_to_dev(sdxi),
+					      sizeof(*l1_table),
+					      &l1_table_dma, GFP_KERNEL);
+		if (!l1_table)
+			return -ENOMEM;
+
+		// Track the L1 table vaddr.
+		sdxi->l1_table_array[l2_idx] = l1_table;
+
+		// Install the new entry in the L2 table.
+		l2_entry = &sdxi->l2_table->entry[l2_idx];
+		set_cxt_l2_entry(l2_entry, l1_table_dma);
+	}
+
+	// Populate the L1 entry.
+	l1_entry = &l1_table->entry[l1_idx];
+	set_cxt_l1_entry(cxt->sdxi, l1_entry, cxt);
+
+	return 0;
+}
+
+static void clear_cxt_table_entries(struct sdxi_cxt_l2_table *l2_table,
+				    struct sdxi_cxt_l1_table *l1_table,
+				    struct sdxi_cxt *cxt)
+{
+	struct sdxi_cxt_l2_ent *l2_entry;
+	struct sdxi_cxt_l1_ent *l1_entry;
+	struct sdxi_dev *sdxi = cxt->sdxi;
+	dma_addr_t l1_dma;
+
+	l2_entry = &l2_table->entry[ID_TO_L2_INDEX(cxt->id)];
+	l1_entry = &l1_table->entry[ID_TO_L1_INDEX(cxt->id)];
+
+	memset(l1_entry, 0, sizeof(*l1_entry));
+
+	// If this L1 table has been completely zeroed then free it.
+	if (memchr_inv(l1_table, 0, L1_TABLE_SIZE))
+		return;
+
+	l1_dma = sdxi_cxt_l2_ent_lv01_ptr(l2_entry);
+
+	memset(l2_entry, 0, sizeof(*l2_entry));
+
+	dma_free_coherent(sdxi_to_dev(sdxi), sizeof(*l1_table),
+			  l1_table, l1_dma);
+}
+
+static void cleanup_cxt_tables(struct sdxi_dev *sdxi,
+			       struct sdxi_cxt *cxt)
+{
+	u16 l2_idx;
+	struct sdxi_cxt_l1_table *l1_table;
+
+	if (!cxt)
+		return;
+
+	l2_idx = ID_TO_L2_INDEX(cxt->id);
+
+	l1_table = sdxi->l1_table_array[l2_idx];
+	/* clear l1 entry */
+	// FIXME combine clear_cxt_table_entries and this function
+	clear_cxt_table_entries(sdxi->l2_table, l1_table, cxt);
+}
+
+static struct sdxi_cxt *alloc_cxt(struct sdxi_dev *sdxi, bool privileged)
+{
+	struct sdxi_cxt *cxt;
+	u16 id, l2_idx, l1_idx;
+
+	if (sdxi->cxt_count >= sdxi->max_cxts)
+		return NULL;
+
+	/* search for an empty context slot */
+	for (id = 0; id < sdxi->max_cxts; id++) {
+		l2_idx = ID_TO_L2_INDEX(id);
+		l1_idx = ID_TO_L1_INDEX(id);
+
+		if (sdxi->cxt_array[l2_idx] == NULL) {
+			int sz = sizeof(struct sdxi_cxt *) * L1_TABLE_ENTRIES;
+			struct sdxi_cxt **ptr = kzalloc(sz, GFP_KERNEL);
+
+			sdxi->cxt_array[l2_idx] = ptr;
+			if (!(sdxi->cxt_array[l2_idx]))
+				return NULL;
+		}
+
+		cxt = (sdxi->cxt_array)[l2_idx][l1_idx];
+		/* found one empty slot */
+		if (!cxt)
+			break;
+	}
+
+	/* nothing found, bail... */
+	if (id == sdxi->max_cxts)
+		return NULL;
+
+	/* alloc context and initialize it */
+	cxt = kzalloc(sizeof(struct sdxi_cxt), GFP_KERNEL);
+	if (!cxt)
+		return NULL;
+
+	cxt->akey_table = dma_alloc_coherent(sdxi_to_dev(sdxi),
+					     sizeof(*cxt->akey_table),
+					     &cxt->akey_table_dma, GFP_KERNEL);
+	if (!cxt->akey_table) {
+		kfree(cxt);
+		return NULL;
+	}
+
+	cxt->sdxi = sdxi;
+	cxt->id = id;
+	cxt->db_base = sdxi->dbs_bar + id * sdxi->db_stride;
+	cxt->db = sdxi->dbs + id * sdxi->db_stride;
+	cxt->privileged = privileged;
+
+	sdxi->cxt_array[l2_idx][l1_idx] = cxt;
+	sdxi->cxt_count++;
+
+	return cxt;
+}
+
+static void free_cxt(struct sdxi_cxt *cxt)
+{
+	struct sdxi_dev *sdxi = cxt->sdxi;
+	u16 l2_idx, l1_idx;
+
+	l2_idx = ID_TO_L2_INDEX(cxt->id);
+	l1_idx = ID_TO_L1_INDEX(cxt->id);
+
+	sdxi->cxt_count--;
+	dma_free_coherent(sdxi_to_dev(sdxi), sizeof(*cxt->akey_table),
+			  cxt->akey_table, cxt->akey_table_dma);
+	kfree(cxt);
+
+	(sdxi->cxt_array)[l2_idx][l1_idx] = NULL;
+}
+
+/* alloc context resources and populate context table */
+static struct sdxi_cxt *sdxi_cxt_alloc(struct sdxi_dev *sdxi, bool privileged)
+{
+	struct sdxi_cxt *cxt;
+
+	mutex_lock(&sdxi->cxt_lock);
+
+	cxt = alloc_cxt(sdxi, privileged);
+	if (!cxt)
+		goto drop_cxt_lock;
+
+	cxt->cxt_ctl = dma_pool_zalloc(sdxi->cxt_ctl_pool, GFP_KERNEL,
+				       &cxt->cxt_ctl_dma);
+	if (!cxt->cxt_ctl)
+		goto release_cxt;
+
+	if (config_cxt_tables(sdxi, cxt))
+		goto release_cxt_ctl;
+
+	trace_sdxi_create_cxt(sdxi, cxt);
+	mutex_unlock(&sdxi->cxt_lock);
+	return cxt;
+
+release_cxt_ctl:
+	dma_pool_free(sdxi->cxt_ctl_pool, cxt->cxt_ctl, cxt->cxt_ctl_dma);
+release_cxt:
+	free_cxt(cxt);
+drop_cxt_lock:
+	mutex_unlock(&sdxi->cxt_lock);
+	return NULL;
+}
+
+/* clear context table and free context resources */
+void sdxi_cxt_free(struct sdxi_cxt *cxt)
+{
+	struct sdxi_dev *sdxi = cxt->sdxi;
+
+	trace_sdxi_free_cxt(sdxi, cxt);
+
+	mutex_lock(&sdxi->cxt_lock);
+
+	cleanup_cxt_tables(sdxi, cxt);
+	dma_pool_free(sdxi->cxt_ctl_pool, cxt->cxt_ctl, cxt->cxt_ctl_dma);
+	free_cxt(cxt);
+
+	mutex_unlock(&sdxi->cxt_lock);
+}
+
+struct sdxi_cxt *sdxi_working_cxt_init(struct sdxi_dev *sdxi,
+				       enum sdxi_cxt_id id)
+{
+	struct sdxi_cxt *cxt;
+	struct sdxi_sq *sq;
+	bool privileged;
+
+	switch (id) {
+	case SDXI_ANY_CXT_ID: // User context
+		privileged = false;
+		if (force_pr_for_user_contexts)
+			privileged = true;
+		break;
+	default: // kernel context
+		privileged = true;
+		break;
+	}
+
+	cxt = sdxi_cxt_alloc(sdxi, privileged);
+	if (!cxt) {
+		sdxi_err(sdxi, "failed to alloc a new context\n");
+		return NULL;
+	}
+
+	/* check if context ID matches */
+	if (id < SDXI_ANY_CXT_ID && cxt->id != id) {
+		sdxi_err(sdxi, "failed to alloc a context with id=%d\n", id);
+		goto err_cxt_id;
+	}
+
+	sq = sdxi_sq_alloc_default(cxt);
+	if (!sq) {
+		sdxi_err(sdxi, "failed to alloc a submission queue (sq)\n");
+		goto err_sq_alloc;
+	}
+
+	return cxt;
+
+err_sq_alloc:
+err_cxt_id:
+	sdxi_cxt_free(cxt);
+
+	return NULL;
 }
 
 static const char *cxt_sts_state_str(enum cxt_sts_state state)
