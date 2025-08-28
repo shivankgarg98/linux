0b50afe61551211a277f94ad492534132eacc5ff
Nathan Lynch <nathan.lynch@amd.com>
2025-08-28 18:07:28 -0500

    dmaengine: sdxi: sdxi.h: Move sdxi_working_cxt_exit() call tree to context.c
    Reviewers might find it somewhat confusing that context management
    functions live in device.c instead of context.c. We can move the
    context shutdown code in one step; the startup code will come next.
    
    Signed-off-by: Nathan Lynch <nathan.lynch@amd.com>

--- drivers/dma/sdxi/context.c
+++ drivers/dma/sdxi/context.c
@@ -10,6 +10,7 @@
 
 #define pr_fmt(fmt)     "SDXI: " fmt
 
+#include <linux/delay.h>
 #include <linux/dma-direction.h>
 #include <linux/dma-mapping.h>
 #include <linux/io-64-nonatomic-lo-hi.h>
@@ -17,6 +18,8 @@
 #include <linux/wordpart.h>
 
 #include "context.h"
+#include "descriptor.h"
+#include "enqueue.h"
 #include "hw.h"
 #include "sdxi.h"
 #include "trace.h"
@@ -128,4 +131,109 @@ void sdxi_sq_free(struct sdxi_sq *sq)
 struct sdxi_sq *sdxi_sq_alloc_default(struct sdxi_cxt *cxt)
 {
 	return sdxi_sq_alloc(cxt, DEFAULT_DESC_RING_ENTRIES);
+}
+
+static const char *cxt_sts_state_str(enum cxt_sts_state state)
+{
+	static const char *const context_states[] = {
+		[CXTV_STOP_SW]  = "stopped (software)",
+		[CXTV_RUN]      = "running",
+		[CXTV_STOPG_SW] = "stopping (software)",
+		[CXTV_STOP_FN]  = "stopped (function)",
+		[CXTV_STOPG_FN] = "stopping (function)",
+		[CXTV_ERR_FN]   = "error",
+	};
+	const char *str = "unknown";
+
+	switch (state) {
+	case CXTV_STOP_SW:
+	case CXTV_RUN:
+	case CXTV_STOPG_SW:
+	case CXTV_STOP_FN:
+	case CXTV_STOPG_FN:
+	case CXTV_ERR_FN:
+		str = context_states[state];
+	}
+
+	return str;
+}
+
+static void sdxi_cxt_shutdown(struct sdxi_cxt *target_cxt)
+{
+	unsigned long deadline = jiffies + msecs_to_jiffies(1000);
+	struct sdxi_cxt *admin_cxt = target_cxt->sdxi->admin_cxt;
+	struct sdxi_dev *sdxi = target_cxt->sdxi;
+	struct sdxi_cxt_sts *sts = target_cxt->sq->cxt_sts;
+	struct sdxi_desc desc;
+	u16 cxtid = target_cxt->id;
+	struct sdxi_cxt_stop params = {
+		.range = sdxi_cxt_range(cxtid),
+	};
+	enum cxt_sts_state state = sdxi_cxt_sts_state(sts);
+	int err;
+
+	sdxi_dbg(sdxi, "%s entry: context state: %s",
+		 __func__, cxt_sts_state_str(state));
+
+	err = sdxi_encode_cxt_stop(&desc, &params);
+	err = sdxi_enqueue(&desc.qw[0], 1,
+			   (__le64 *)admin_cxt->sq->desc_ring,
+			   admin_cxt->sq->ring_entries,
+			   &admin_cxt->sq->cxt_sts->read_index,
+			   admin_cxt->sq->write_index, admin_cxt->db);
+	if (err) {
+		sdxi_err(sdxi, "error %d shutting down context %u\n",
+			 err, cxtid);
+		return;
+	}
+
+	sdxi_dbg(sdxi, "shutting down context %u\n", cxtid);
+
+	do {
+		enum cxt_sts_state state = sdxi_cxt_sts_state(sts);
+
+		sdxi_dbg(sdxi, "context %u state: %s", cxtid,
+			 cxt_sts_state_str(state));
+
+		switch (state) {
+		case CXT_STATE_ERR:
+			sdxi_err(sdxi, "context %u went into error state while stopping\n",
+				cxtid);
+			fallthrough;
+		case CXTV_STOP_SW:
+		case CXTV_STOP_FN:
+			return;
+		case CXTV_RUN:
+		case CXTV_STOPG_SW:
+		case CXTV_STOPG_FN:
+			// transitional states
+			fsleep(1000);
+			break;
+		default:
+			sdxi_err(sdxi, "context %u in unknown state %u\n",
+				 cxtid, state);
+			return;
+		}
+	} while (time_before(jiffies, deadline));
+
+	sdxi_err(sdxi, "stopping context %u timed out (state = %u)\n",
+		cxtid, sdxi_cxt_sts_state(sts));
+}
+
+void sdxi_working_cxt_exit(struct sdxi_cxt *cxt)
+{
+	struct sdxi_sq *sq;
+
+	if (!cxt)
+		return;
+
+	sq = cxt->sq;
+	if (!sq)
+		return;
+
+	sdxi_cxt_shutdown(cxt);
+
+	sdxi_sq_free(sq);
+
+	sdxi_cxt_free(cxt);
 }
