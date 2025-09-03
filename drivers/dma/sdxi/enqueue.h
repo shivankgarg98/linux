2df7950f5eccc13d78df079037d89d22542a5425
Nathan Lynch <nathan.lynch@amd.com>
2025-09-03 14:46:23 -0500

    dmaengine: sdxi: Use consistent comment style (enqueue.h)
    Signed-off-by: Nathan Lynch <nathan.lynch@amd.com>

--- drivers/dma/sdxi/enqueue.h
+++ drivers/dma/sdxi/enqueue.h
@@ -4,12 +4,12 @@
 
 #include <linux/types.h>
 
-int sdxi_enqueue(const __le64 *enq_entries, // Ptr to entries to enqueue
-		 u64 enq_num, // Number of entries to enqueue
-		 __le64 *ring_base, // Ptr to ring location
-		 u64 ring_size, // (Ring Size in bytes)/64
-		 __le64 const volatile * const Read_Index, // Ptr to Read_Index location
-		 __le64 volatile * const Write_Index, // Ptr to Write_Index location
-		 __le64 __iomem *Door_Bell); // Ptr to Ring Doorbell location
+int sdxi_enqueue(const __le64 *enq_entries,  /* Ptr to entries to enqueue */
+		 u64 enq_num,  /* Number of entries to enqueue */
+		 __le64 *ring_base,  /* Ptr to ring location */
+		 u64 ring_size,  /* (Ring Size in bytes)/64 */
+		 __le64 const volatile * const Read_Index,  /* Ptr to Read_Index location */
+		 __le64 volatile * const Write_Index,  /* Ptr to Write_Index location */
+		 __le64 __iomem *Door_Bell);  /* Ptr to Ring Doorbell location */
 
 #endif /* DMA_SDXI_ENQUEUE_H */
