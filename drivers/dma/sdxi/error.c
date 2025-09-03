753575dd7cec886c93402e00e0bf5de2430ed81b
Nathan Lynch <nathan.lynch@amd.com>
2025-09-03 14:43:53 -0500

    dmaengine: sdxi: Use consistent comment style (error.c)
    Signed-off-by: Nathan Lynch <nathan.lynch@amd.com>

--- drivers/dma/sdxi/error.c
+++ drivers/dma/sdxi/error.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only
-// SDXI error reporting.
+/* SDXI error reporting. */
 
 #include <linux/interrupt.h>
 #include <linux/irqreturn.h>
@@ -10,12 +10,14 @@
 #include "mmio.h"
 #include "sdxi.h"
 
-// The error log ring buffer size is configurable, but for now we fix
-// it to 64 entries (which is the spec minimum).
+/*
+ * The error log ring buffer size is configurable, but for now we fix
+ * it to 64 entries (which is the spec minimum).
+ */
 #define ERROR_LOG_ENTRIES 64
 #define ERROR_LOG_SZ (ERROR_LOG_ENTRIES * sizeof(struct sdxi_errlog_hd_ent))
 
-// The "unpacked" counterpart to ERRLOG_HD_ENT.
+/* The "unpacked" counterpart to ERRLOG_HD_ENT. */
 struct errlog_entry {
 	u64 dsc_index;
 	u16 cxt_num;
@@ -36,7 +38,7 @@ struct errlog_entry {
 #define ERRLOG_ENTRY_FLAG(nr_, name_) \
 	ERRLOG_ENTRY_FIELD(nr_, nr_, name_)
 
-// Refer to "Error Log Header Entry (ERRLOG_HD_ENT)"
+/* Refer to "Error Log Header Entry (ERRLOG_HD_ENT)" */
 static const struct packed_field_u16 errlog_hd_ent_fields[] = {
 	ERRLOG_ENTRY_FLAG(0, vl),
 	ERRLOG_ENTRY_FIELD(13, 8, step),
@@ -56,8 +58,10 @@ enum {
 	SDXI_PACKING_QUIRKS = QUIRK_LITTLE_ENDIAN | QUIRK_LSW32_IS_FIRST,
 };
 
-// Refer to "(Flagged) Processing Step" and
-// "Error Log Header Entry (ERRLOG_HD_ENT)", subfield "step"
+/*
+ * Refer to "(Flagged) Processing Step" and
+ * "Error Log Header Entry (ERRLOG_HD_ENT)", subfield "step"
+ */
 enum errv_step {
 	ERRV_INT         = 1,
 	ERRV_CXT_L2      = 2,
@@ -101,7 +105,7 @@ static const char *step_str(enum errv_step step)
 	return str;
 }
 
-// Refer to "Error Log Header Entry (ERRLOG_HD_ENT)", subfield "sub_step"
+/* Refer to "Error Log Header Entry (ERRLOG_HD_ENT)", subfield "sub_step" */
 enum errv_sub_step {
 	SUB_STEP_OTHER    = 0,
 	SUB_STEP_ATF      = 1,
@@ -129,7 +133,7 @@ static const char *sub_step_str(enum errv_sub_step sub_step)
 	return str;
 }
 
-// Refer to "Error Log Header Entry (ERRLOG_HD_ENT)", subfield "re"
+/* Refer to "Error Log Header Entry (ERRLOG_HD_ENT)", subfield "re" */
 enum fn_reaction {
 	FN_REACT_INFORM      = 0,
 	FN_REACT_CXT_STOP    = 1,
@@ -194,7 +198,7 @@ static void sdxi_print_err(struct sdxi_dev *sdxi, u64 err_rd)
 	sdxi_err(sdxi, "  err_class: %#x\n", ent.err_class);
 }
 
-// Refer to "Error Log Processing by Software"
+/* Refer to "Error Log Processing by Software" */
 static irqreturn_t sdxi_irq_thread(int irq, void *data)
 {
 	struct sdxi_dev *sdxi = data;
@@ -202,15 +206,17 @@ static irqreturn_t sdxi_irq_thread(int irq, void *data)
 	u64 read_index;
 	u64 err_sts;
 
-	// 1. Check MMIO_ERR_STS and perform any required remediation.
+	/* 1. Check MMIO_ERR_STS and perform any required remediation. */
 	err_sts = sdxi_read64(sdxi, SDXI_MMIO_ERR_STS);
 	if (!(err_sts & SDXI_MMIO_ERR_STS_STS_BIT))
 		return IRQ_HANDLED;
 
 	if (err_sts & SDXI_MMIO_ERR_STS_ERR_BIT) {
-		// Assume this isn't recoverable; e.g. the error log
-		// isn't configured correctly. Don't clear
-		// SDXI_MMIO_ERR_STS before returning.
+		/*
+		 * Assume this isn't recoverable; e.g. the error log
+		 * isn't configured correctly. Don't clear
+		 * SDXI_MMIO_ERR_STS before returning.
+		 */
 		sdxi_err(sdxi, "attempted but failed to log errors\n");
 		sdxi_err(sdxi, "error log not functional\n");
 		return IRQ_HANDLED;
@@ -219,87 +225,95 @@ static irqreturn_t sdxi_irq_thread(int irq, void *data)
 	if (err_sts & SDXI_MMIO_ERR_STS_OVF_BIT)
 		sdxi_err(sdxi, "error log overflow, some entries lost\n");
 
-	// 2. If MMIO_ERR_STS.sts is 1, then compute read_index.
+	/* 2. If MMIO_ERR_STS.sts is 1, then compute read_index. */
 	read_index = sdxi_read64(sdxi, SDXI_MMIO_ERR_RD);
 
-	// 3. Clear MMIO_ERR_STS. The flags in this register are RW1C.
+	/* 3. Clear MMIO_ERR_STS. The flags in this register are RW1C. */
 	sdxi_write64(sdxi, SDXI_MMIO_ERR_STS,
 		     SDXI_MMIO_ERR_STS_STS_BIT |
 		     SDXI_MMIO_ERR_STS_OVF_BIT |
 		     SDXI_MMIO_ERR_STS_ERR_BIT);
 
-	// 4. Compute write_index.
+	/* 4. Compute write_index. */
 	write_index = sdxi_read64(sdxi, SDXI_MMIO_ERR_WRT);
 
-	// 5. If the indexes are equal then exit.
+	/* 5. If the indexes are equal then exit. */
 	if (read_index == write_index)
 		return IRQ_HANDLED;
 
-	// 6. While read_index < write_index...
+	/* 6. While read_index < write_index... */
 	while (read_index < write_index) {
 
-		// 7. and 8. Compute the real ring buffer index from
-		// read_index and process the entry.
+		/*
+		 * 7. and 8. Compute the real ring buffer index from
+		 * read_index and process the entry.
+		 */
 		sdxi_print_err(sdxi, read_index);
 
-		// 9. Advance read_index.
+		/* 9. Advance read_index. */
 		++read_index;
 
-		// 10. Return to step 6.
+		/* 10. Return to step 6. */
 	}
 
-	// 11. Write read_index to MMIO_ERR_RD.
+	/* 11. Write read_index to MMIO_ERR_RD. */
 	sdxi_write64(sdxi, SDXI_MMIO_ERR_RD, read_index);
 
 	return IRQ_HANDLED;
 }
 
-// Refer to "Error Log Initialization"
+/* Refer to "Error Log Initialization" */
 int sdxi_error_init(struct sdxi_dev *sdxi)
 {
 	u64 reg;
 	int err;
 
-	// 1. Clear MMIO_ERR_CFG. Error interrupts are inhibited until step 6.
+	/* 1. Clear MMIO_ERR_CFG. Error interrupts are inhibited until step 6. */
 	sdxi_write64(sdxi, SDXI_MMIO_ERR_CFG, 0);
 
-	// 2. Clear MMIO_ERR_STS. The flags in this register are RW1C.
+	/* 2. Clear MMIO_ERR_STS. The flags in this register are RW1C. */
 	reg = FIELD_PREP(SDXI_MMIO_ERR_STS_STS_BIT, 1) |
 	      FIELD_PREP(SDXI_MMIO_ERR_STS_OVF_BIT, 1) |
 	      FIELD_PREP(SDXI_MMIO_ERR_STS_ERR_BIT, 1);
 	sdxi_write64(sdxi, SDXI_MMIO_ERR_STS, reg);
 
-	// 3. Allocate memory for the error log ring buffer, initialize to zero.
+	/* 3. Allocate memory for the error log ring buffer, initialize to zero. */
 	sdxi->err_log = dma_alloc_coherent(sdxi_to_dev(sdxi), ERROR_LOG_SZ,
 					   &sdxi->err_log_dma, GFP_KERNEL);
 	if (!sdxi->err_log)
 		return -ENOMEM;
 
-	// 4. Set MMIO_ERR_CTL.intr_en to 1 if interrupts on
-	// context-level errors are desired.
+	/*
+	 * 4. Set MMIO_ERR_CTL.intr_en to 1 if interrupts on
+	 * context-level errors are desired.
+	 */
 	reg = sdxi_read64(sdxi, SDXI_MMIO_ERR_CTL);
 	FIELD_MODIFY(SDXI_MMIO_ERR_CTL_EN, &reg, 1);
 	sdxi_write64(sdxi, SDXI_MMIO_ERR_CTL, reg);
 
-	// The spec is not explicit about when to do this, but this
-	// seems like the right time: enable interrupt on
-	// function-level transition to error state.
+	/*
+	 * The spec is not explicit about when to do this, but this
+	 * seems like the right time: enable interrupt on
+	 * function-level transition to error state.
+	 */
 	reg = sdxi_read64(sdxi, SDXI_MMIO_CTL0);
 	FIELD_MODIFY(SDXI_MMIO_CTL0_FN_ERR_INTR_EN, &reg, 1);
 	sdxi_write64(sdxi, SDXI_MMIO_CTL0, reg);
 
-	// 5. Clear MMIO_ERR_WRT and MMIO_ERR_RD.
+	/* 5. Clear MMIO_ERR_WRT and MMIO_ERR_RD. */
 	sdxi_write64(sdxi, SDXI_MMIO_ERR_WRT, 0);
 	sdxi_write64(sdxi, SDXI_MMIO_ERR_RD, 0);
 
-	// Error interrupts can be generated once MMIO_ERR_CFG.en is
-	// set in step 6, so set up the handler now.
+	/*
+	 * Error interrupts can be generated once MMIO_ERR_CFG.en is
+	 * set in step 6, so set up the handler now.
+	 */
 	err = request_threaded_irq(sdxi->error_irq, NULL, sdxi_irq_thread,
 				   IRQF_TRIGGER_NONE, "SDXI error", sdxi);
 	if (err)
 		goto free_errlog;
 
-	// 6. Program MMIO_ERR_CFG.
+	/* 6. Program MMIO_ERR_CFG. */
 	reg = FIELD_PREP(SDXI_MMIO_ERR_CFG_PTR, sdxi->err_log_dma >> 12) |
 	      FIELD_PREP(SDXI_MMIO_ERR_CFG_SZ, ERROR_LOG_ENTRIES >> 6) |
 	      FIELD_PREP(SDXI_MMIO_ERR_CFG_EN, 1);
