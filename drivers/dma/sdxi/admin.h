2cdfc64469d71e1259c04bd1d8abd8c7e840a360
Nathan Lynch <nathan.lynch@amd.com>
2025-12-18 17:40:41 -0600

    dmaengine: sdxi: admin: Remove unnecessary forward declarations
    Only sdxi_cxt is needed here.
    
    Signed-off-by: Nathan Lynch <nathan.lynch@amd.com>

--- drivers/dma/sdxi/admin.h
+++ drivers/dma/sdxi/admin.h
@@ -1,10 +1,7 @@
 #ifndef LINUX_SDXI_ADMIN_H
 #define LINUX_SDXI_ADMIN_H
 
-struct sdxi_dev;
 struct sdxi_cxt;
-struct sdxi_cxt_start;
-struct sdxi_cxt_stop;
 
 int sdxi_adm_start_cxt(struct sdxi_cxt *cxt);
 void sdxi_adm_stop_cxt(struct sdxi_cxt *cxt);
